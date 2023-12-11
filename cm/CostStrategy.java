dd8e2bfdc0055a9a9daa93dc570c1327a02d0c03
N090 <91678195+N090@users.noreply.github.com>
2023-12-11 02:08:31 +0000

    Changing from abstact class to interface

--- cm/CostStrategy.java
+++ cm/CostStrategy.java
@@ -2,7 +2,6 @@ package cm;
 
 import java.math.BigDecimal;
 
-public abstract class CostStrategy {
-
-    abstract BigDecimal modify(BigDecimal standardRate);
-}
+public interface CostStrategy
+{
+    public BigDecimal CostBehaviour(BigDecimal amount);}
--- cm/VisitorCost.java
+++ cm/VisitorCost.java
@@ -2,17 +2,17 @@ package cm;
 
 import java.math.BigDecimal;
 
-public class VisitorCost extends CostStrategy {
+class VisitorCost implements CostStrategy {
 
     @Override
-    BigDecimal modify(BigDecimal rate) {
-        rate = rate.subtract(BigDecimal.valueOf(10));
-        if(rate.compareTo(BigDecimal.valueOf(0)) > 0) {
-            rate = BigDecimal.valueOf(rate.intValue() / 2.0);
-            return rate;
-        }
-        else {
+    public BigDecimal CostBehaviour(BigDecimal amount) {
+        if (amount.doubleValue() <= 10) {
             return BigDecimal.valueOf(0);
         }
+        else {
+            return amount.subtract(BigDecimal.valueOf(10)).multiply(BigDecimal.valueOf(0.5));
+        }
+
     }
 }
+
